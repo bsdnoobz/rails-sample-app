44820387e655e355fc9a26494329b78f3ddc2a41
Nash <nash@bsd-noobz.com>
2014-10-26 23:10:48 +0700

    minor fixes

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
+require 'simplecov'
+require 'simplecov-csv'
+SimpleCov.formatter = SimpleCov::Formatter::CSVFormatter
+SimpleCov.coverage_dir(ENV['COVERAGE_REPORTS'])
+SimpleCov.start
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
